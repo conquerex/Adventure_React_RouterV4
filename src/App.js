362f9c30e5cbd22ff8d13942a711cf74c0aafaa3
손종국 <conquerex@gmail.com>
2018-05-09 15:06:16 +0900

    Use Link and Match

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import {BrowserRouter as Router, Link, Match, Miss} from 'react-router';
+import Home from './Home'
+import About from './About'
+import Topics from './Topics'
 
 const App = () => {
   return (
     <Router>
       <div>
         <ul>
-        <li><Link to = "/">Home</Link></li>
+          <li><Link to = "/">Home</Link></li>
+          <li><Link to = "/About">About</Link></li>
+          <li><Link to = "/Topics">Topics</Link></li>
         </ul>
+        <hr/>
+        <Match exactly pattern='/' component={Home}/>
+        <Match pattern='/about' component={About}/>
+        <Match pattern='/topics' component={Topics}/>
       </div>
     </Router>
   );
