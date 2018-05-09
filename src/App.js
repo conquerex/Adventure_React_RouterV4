0afd85d71c02da4424bab0b216126f9e413d5872
손종국 <conquerex@gmail.com>
2018-05-09 18:50:16 +0900

    Not found page by Switch and location

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {BrowserRouter as Router, Link, Route, Miss} from 'react-router-dom';
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 import Home from './Home'
 import About from './About'
 import Topics from './Topics'
+import NotFound from './NotFound'
 
 const App = () => {
   return (
@@ -14,9 +15,12 @@ const App = () => {
           <li><Link to = "/Topics">Topics</Link></li>
         </ul>
         <hr/>
-        <Route exact path='/' component={Home}/>
-        <Route path='/about' component={About}/>
-        <Route path='/topics' component={Topics}/>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/about' component={About}/>
+          <Route path='/topics' component={Topics}/>
+          <Route component={NotFound}/>
+        </Switch>
       </div>
     </Router>
   );
